1a96e04105360e577d7c363290f4a30e9bf554a9
Khoa Ngô <nndkhoa9@gmail.com>
2021-08-01 11:50:51 +0700

    add method for substraction 2 numbers

--- src/main/java/com/onemount/App.java
+++ src/main/java/com/onemount/App.java
@@ -8,6 +8,10 @@ public class App {
     return a + b;
   }
 
+  public static int substract(int a, int b) {
+    return a - b;
+  }
+
   public static void main(String[] args) {
     System.out.println("Hello World!");
 
